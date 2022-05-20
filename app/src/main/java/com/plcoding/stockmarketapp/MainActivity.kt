5dd5b866a2a8c9e74b1c609cbf240aa49a30c3b2
Sumit Kumar <sumit.k@ingenium.com>
2022-05-20 18:17:21 +0530

    commit   final

--- app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
@@ -3,23 +3,31 @@ package com.plcoding.stockmarketapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.plcoding.stockmarketapp.presentation.NavGraphs
+import com.plcoding.stockmarketapp.presentation.company_listings.CompanyListingsViewModel
 import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+       installSplashScreen()
         setContent {
             StockMarketAppTheme {
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
